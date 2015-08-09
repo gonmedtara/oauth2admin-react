68b9fc4255fd68adcda8970d4bb549489ecca60a
Anatoly Lagodich <alagodich@northernlight.com>
2015-08-09 15:49:02 -0400

    Oauth 2 clients CRUD complete
    Widget is build to work with Filsh/yii2-oauth2-server
    @see https://github.com/Filsh/yii2-oauth2-server

--- src/clients.js
+++ src/clients.js
@@ -1,16 +1,22 @@
-// -ClientsCRUD
-//   -SearchBar
-//   -ClientList
-//     -Client
-//       -ClientActions
-//       -ClientDetails (Modal)
-//     -NewClientForm (Modal)
+/**
+ * Hierarchy
+ *
+ * -ClientsCRUD
+ *   -SearchBar
+ *   -ClientList
+ *     -Client
+ *       -ClientActions
+ *          -ClientUpdate
+ *          -ClientDelete
+ *       -ClientDetails (Modal)
+ */
 
 var ClientsCRUD = React.createClass({
     getInitialState: function() {
         return {
             clients: [],
-            filterText: ''
+            filterText: '',
+            openNewClientForm: false
         };
     },
     handleUserInput: function(filterText) {
@@ -32,13 +38,13 @@ var ClientsCRUD = React.createClass({
             }.bind(this)
         });
     },
-    handleUpdate: function(data) {
+    handleNew: function(data) {
         $.ajax({
             url: this.props.url,
             dataType: 'json',
             type: 'post',
-            success: function(data) {
-                console.log(data);
+            data: {'client': data},
+            success: function() {
                 this.loadClients();
             }.bind(this),
             error: function(xhr, status, error) {
@@ -46,15 +52,62 @@ var ClientsCRUD = React.createClass({
             }.bind(this)
         });
     },
+    handleUpdate: function(data) {
+        $.ajax({
+            url: this.props.url + '/' + data.client_id,
+            dataType: 'json',
+            type: 'patch',
+            data: {'client': data},
+            success: function() {
+                this.loadClients();
+            }.bind(this),
+            error: function(xhr, status, error) {
+                console.log(xhr, status, error);
+            }.bind(this)
+        });
+    },
+    handleDelete: function(client_id) {
+        $.ajax({
+            url: this.props.url + '/' + client_id,
+            dataType: 'json',
+            type: 'delete',
+            success: function() {
+                this.loadClients();
+            }.bind(this),
+            error: function(xhr, status, error) {
+                console.log(xhr, status, error);
+            }.bind(this),
+        });
+    },
+    toggleNewClientForm: function() {
+        this.setState({openNewClientForm: !this.state.openNewClientForm});
+    },
     render: function() {
+        var controllsStyle = {
+            padding: 0,
+            margin: 0
+        };
+        var newClientData = {
+            client_id: '',
+            client_secret: '',
+            grant_types: 'password'
+        };
         return (
             <div className="clientsCRUD">
+                <ClientNew
+                    data={newClientData}
+                    open={this.state.openNewClientForm}
+                    handler={this.toggleNewClientForm}
+                    onSubmit={this.handleNew}
+                />
                 <SearchBar filterText={this.state.filterText} onUserInput={this.handleUserInput} />
                 <ClientList
                     clients={this.state.clients}
                     filterText={this.state.filterText}
                     onUpdate={this.handleUpdate}
+                    onDelete={this.handleDelete}
                 />
+                <button className="ui right primary button" onClick={this.toggleNewClientForm}>Create</button>
             </div>
         );
     }
@@ -67,7 +120,6 @@ var SearchBar = React.createClass({
     render: function() {
         return (
             <div className="searchBar">
-                <button className="ui right floated small primary button">Create</button>
                 <div className="ui small form">
                     <div className="six wide inline field">
                         <input
@@ -92,7 +144,12 @@ var ClientList = React.createClass({
             if (client.client_id.indexOf(filterText) == -1) {
                 return;
             }
-            items.push(<Client key={client.client_id} data={client} onUpdate={this.props.onUpdate}/>);
+            items.push(<Client
+                key={client.client_id}
+                data={client}
+                onUpdate={this.props.onUpdate}
+                onDelete={this.props.onDelete}
+            />);
         }.bind(this));
         return (
             <div className="clientList ui middle aligned divided list">
@@ -106,7 +163,8 @@ var Client = React.createClass({
     getInitialState: function() {
         return {
             detailsOpen: false,
-            updateOpen: false
+            updateOpen: false,
+            deleteOpen: false
         };
     },
     toggleDetails: function() {
@@ -114,6 +172,9 @@ var Client = React.createClass({
     },
     toggleUpdate: function() {
         this.setState({updateOpen: !this.state.updateOpen});
+    },
+    toggleDelete: function() {
+        this.setState({deleteOpen: !this.state.deleteOpen});
     },
     render: function() {
         var data = this.props.data;
@@ -125,6 +186,9 @@ var Client = React.createClass({
                         updateOpen={this.state.updateOpen}
                         data={data}
                         onUpdate={this.props.onUpdate}
+                        deleteOpen={this.state.deleteOpen}
+                        deleteHandler={this.toggleDelete}
+                        onDelete={this.props.onDelete}
                     />
                 </div>
                 <div className="content" onClick={this.toggleDetails}>
@@ -138,9 +202,6 @@ var Client = React.createClass({
 });
 
 var ClientActions = React.createClass({
-    handleUpdate: function(data) {
-        this.props.onUpdate(data);
-    },
     render: function() {
         return (
             <div className="clientActions ui horizontal divided list">
@@ -153,13 +214,19 @@ var ClientActions = React.createClass({
                     data={this.props.data}
                     open={this.props.updateOpen}
                     handler={this.props.updateHandler}
-                    onSubmit={this.handleUpdate}
+                    onSubmit={this.props.onUpdate}
                 />
-                <div className="item">
+                <div className="item" onClick={this.props.deleteHandler}>
                     <div className="content">
                         <i className="ban red icon"></i>
                     </div>
                 </div>
+                <ClientDelete
+                    data={this.props.data}
+                    open={this.props.deleteOpen}
+                    handler={this.props.deleteHandler}
+                    onSubmit={this.props.onDelete}
+                />
             </div>
         );
     }
@@ -188,7 +255,7 @@ var ClientDetails = React.createClass({
                     <div className="description ui list">
                         <div className="item">Id: <strong>{data.client_id}</strong></div>
                         <div className="item">secret: <strong>{data.client_secret}</strong></div>
-                        <div className="item">redirect url: <strong>{data.redirect_url}</strong></div>
+                        <div className="item">redirect uri: <strong>{data.redirect_uri}</strong></div>
                         <div className="item">grant types: <strong>{data.grant_types}</strong></div>
                         <div className="item">scope: <strong>{data.scope}</strong></div>
                     </div>
@@ -202,6 +269,32 @@ var ClientDetails = React.createClass({
 });
 
 var ClientUpdate = React.createClass({
+    render: function() {
+        return (
+            <ClientForm
+                data={this.props.data}
+                onSubmit={this.props.onSubmit}
+                open={this.props.open}
+                handler={this.props.handler}
+            />
+        );
+    }
+});
+
+var ClientNew = React.createClass({
+    render: function() {
+        return (
+            <ClientForm
+                data={this.props.data}
+                onSubmit={this.props.onSubmit}
+                open={this.props.open}
+                handler={this.props.handler}
+            />
+        );
+    }
+});
+
+var ClientForm = React.createClass({
     componentDidMount: function() {
         $(React.findDOMNode(this.refs.modal)).modal({
             // transition: 'vertical flip',
@@ -219,14 +312,14 @@ var ClientUpdate = React.createClass({
     handleSubmit: function() {
         var client_id = React.findDOMNode(this.refs.client_id).value.trim();
         var client_secret = React.findDOMNode(this.refs.client_secret).value.trim();
-        var redirect_url = React.findDOMNode(this.refs.redirect_url).value.trim();
+        var redirect_uri = React.findDOMNode(this.refs.redirect_uri).value.trim();
         var grant_types = React.findDOMNode(this.refs.grant_types).value.trim();
         var scope = React.findDOMNode(this.refs.scope).value.trim();
 
         this.props.onSubmit({
             client_id: client_id,
             client_secret: client_secret,
-            redirect_url: redirect_url,
+            redirect_uri: redirect_uri,
             grant_types: grant_types,
             scope: scope
         });
@@ -234,23 +327,25 @@ var ClientUpdate = React.createClass({
     },
     render: function() {
         var data = this.props.data;
+        var isNew = !data.client_id;
+        var submitActionName = isNew ? 'Create' : 'Update';
         return (
             <div className="clientForm ui small modal" ref="modal">
                 <div className="header">
-                    Update {this.props.data.client_id}
+                    Update {data.client_id}
                 </div>
                 <div className="ui form content">
                     <div className="field">
                         <label>Client id</label>
-                        <input type="text" defaultValue={data.client_id} ref="client_id" readOnly={true} />
+                        <input type="text" defaultValue={data.client_id} ref="client_id" readOnly={!isNew} />
                     </div>
                     <div className="field">
                         <label>Client secret</label>
                         <input type="text" defaultValue={data.client_secret} ref="client_secret" />
                     </div>
                     <div className="field">
-                        <label>Redirect Url</label>
-                        <input type="text" defaultValue={data.redirect_url} ref="redirect_url" />
+                        <label>Redirect Uri</label>
+                        <input type="text" defaultValue={data.redirect_uri} ref="redirect_uri" />
                     </div>
                     <div className="field">
                         <label>Grant types</label>
@@ -262,7 +357,7 @@ var ClientUpdate = React.createClass({
                     </div>
                 </div>
                 <div className="actions">
-                    <button className="ui small primary button" onClick={this.handleSubmit}>Update</button>
+                    <input type="submit" value={submitActionName} className="ui small primary button" onClick={this.handleSubmit} />
                     <div className="ui cancel button">Cancel</div>
                 </div>
             </div>
@@ -270,5 +365,48 @@ var ClientUpdate = React.createClass({
     }
 });
 
+var ClientDelete = React.createClass({
+    componentDidMount: function() {
+        $(React.findDOMNode(this.refs.modal)).modal({
+            // transition: 'vertical flip',
+            detachable: false,
+            onHide: function() {
+                this.props.handler();
+            }.bind(this)
+        });
+    },
+    componentDidUpdate: function() {
+        if (this.props.open) {
+            $(React.findDOMNode(this.refs.modal)).modal('show');
+        }
+    },
+    handleSubmit: function() {
+        this.props.onSubmit(this.props.data.client_id);
+    },
+    render: function() {
+        var data = this.props.data;
+        return (
+            <div className='clientDelete ui small basic modal' ref="modal">
+                <div className="ui icon header">
+                    <i className="archive icon"></i>
+                    Delete this client
+                </div>
+                <div className="content">
+                    <p>Are you sure that you want to delete {data.client_id}?</p>
+                </div>
+                <div className="actions">
+                    <div className="ui red basic cancel inverted button">
+                        <i className="remove icon"></i>
+                        No
+                    </div>
+                    <div className="ui green ok inverted button" onClick={this.handleSubmit}>
+                        <i className="checkmark icon"></i>
+                        Yes
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
 
-React.render(<ClientsCRUD url="/oauth2-admin/client"/>, document.getElementById('clientsContainer'));
+React.render(<ClientsCRUD url="/oauth2-admin/clients"/>, document.getElementById('clientsContainer'));
